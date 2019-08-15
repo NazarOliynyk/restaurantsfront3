9692ba23ccae233be55f4583f6707f560dad0757
Your Name <nazar_lw@ukr.net>
2019-08-15 13:51:32 +0300

    url in services changed for AWS, new  favicon

--- src/app/controllerServices/restaurant-controller.service.ts
+++ src/app/controllerServices/restaurant-controller.service.ts
@@ -12,7 +12,8 @@ import {ResponseTransfer} from '../Models/ResponseTransfer';
 })
 export class RestaurantControllerService {
 
-  url = 'http://localhost:8080';
+  // url = 'http://localhost:8080';
+  url = 'http://ec2-18-222-130-33.us-east-2.compute.amazonaws.com:8080';
 
   constructor(
     private http: HttpClient
